363de8f0fc0ab84450ea17d65635558f6dde0fe3
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2018-11-27 00:58:33 +0800

    Update and rename lstm_imdb_vib.py to cnn_imdb_vib.py

--- cnn_imdb_vib.py
+++ cnn_imdb_vib.py
@@ -18,14 +18,20 @@ class VIB(Layer):
         u = K.random_normal(shape=K.shape(z_mean))
         kl_loss = - 0.5 * K.sum(K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), 0))
         self.add_loss(self.lamb * kl_loss)
+        u = K.in_train_phase(u, 0.)
         return z_mean + K.exp(z_log_var / 2) * u
     def compute_output_shape(self, input_shape):
         return input_shape[0]
 
 
-max_features = 20000
-maxlen = 80
+max_features = 5000
+maxlen = 400
 batch_size = 32
+embedding_dims = 50
+filters = 250
+kernel_size = 3
+epochs = 5
+
 
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
@@ -35,11 +41,17 @@ x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 seq_in = Input(shape=(maxlen,))
 seq = seq_in
 
-seq = Embedding(max_features, 128)(seq)
-seq = LSTM(128)(seq)
+seq = Embedding(max_features, embedding_dims)(seq)
+seq = Conv1D(filters,
+             kernel_size,
+             padding='valid',
+             activation='relu',
+             strides=1)(seq)
+seq = GlobalMaxPooling1D()(seq)
+
 z_mean = Dense(128)(seq)
 z_log_var = Dense(128)(seq)
-seq = VIB(0.001)([z_mean, z_log_var])
+seq = VIB(0.1)([z_mean, z_log_var])
 seq = Dense(1, activation='sigmoid')(seq)
 
 model = Model(seq_in, seq)
@@ -49,16 +61,23 @@ model.compile(loss='binary_crossentropy',
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
-          epochs=15,
+          epochs=epochs,
           validation_data=(x_test, y_test))
 
 
 """
+# 去掉VIB后的模型
+
 seq_in = Input(shape=(maxlen,))
 seq = seq_in
 
-seq = Embedding(max_features, 128)(seq)
-seq = LSTM(128)(seq)
+seq = Embedding(max_features, embedding_dims)(seq)
+seq = Conv1D(filters,
+             kernel_size,
+             padding='valid',
+             activation='relu',
+             strides=1)(seq)
+seq = GlobalMaxPooling1D()(seq)
 seq = Dense(1, activation='sigmoid')(seq)
 
 model = Model(seq_in, seq)
@@ -68,6 +87,6 @@ model.compile(loss='binary_crossentropy',
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
-          epochs=15,
+          epochs=epochs,
           validation_data=(x_test, y_test))
 """
